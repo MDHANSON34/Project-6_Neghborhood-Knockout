1652c7741c5601e58ae7eb69c132324922ebca5d
MDHANSON34 <mh820b@att.com>
2018-03-07 16:54:07 -0800

    Add files via upload

---
+++ Project 6_Neghborhood Knockout/js/markers.js
@@ -0,0 +1,8 @@
+var locations = [
+  {title: 'Half Dome', location: {lat: 37.746036, lng: -119.53294}},
+  {title: 'Yosemite Falls', location: {lat: 37.749896, lng: -119.595933}},
+  {title: 'Glacier Point', location: {lat: 37.729041, lng: -119.574772}},
+  {title: 'Sentinel Dome', location: {lat: 37.722895, lng: -119.584669}},
+  {title: 'El Capitan', location: {lat: 37.733263, lng: -119.637898}},
+  {title: 'Clouds Rest', location: {lat: 37.767328, lng: -119.489431}}
+];
