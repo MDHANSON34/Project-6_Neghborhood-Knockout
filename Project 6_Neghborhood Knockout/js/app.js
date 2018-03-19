46ab1c91e7b1127ad20a0a517a1e4e8e9bde7024
MDHANSON34 <mh820b@att.com>
2018-03-19 16:20:54 -0700

    Add files via upload

--- Project 6_Neghborhood Knockout/js/app.js
+++ Project 6_Neghborhood Knockout/js/app.js
@@ -1,281 +1,131 @@
-  var map;
-  // Create a new blank array for all the listing markers.
-  var markers = [];
-  // Create placemarkers array to use in multiple functions to have control
-  // over the number of places that show.
-  var placeMarkers = [];
+var map;
+var allMarkers=[];
+var markers = [
+  {title: 'half dome', position: {lat: 37.746036, lng: -119.53294}, id: 0},
+  {title: 'yosemite falls', position: {lat: 37.749896, lng: -119.595933}, id: 1},
+  {title: 'glacier point', position: {lat: 37.729041, lng: -119.574772}, id: 3},
+  {title: 'sentinel dome', position: {lat: 37.722895, lng: -119.584669}, id: 4},
+  {title: 'el capitan', position: {lat: 37.733263, lng: -119.637898}, id: 5},
+  {title: 'clouds rest', position: {lat: 37.767328, lng: -119.489431}, id: 6}
+];
 
-  function initMap() {
-    // Create a styles array to use with the map.
-    var styles = [{
-      featureType: 'water',
-      stylers: [{
-        color: '#19a0d8'
-      }]
-    }, {
-      featureType: 'administrative',
-      elementType: 'labels.text.stroke',
-      stylers: [{
-          color: '#ffffff'
-        },
-        {
-          weight: 6
-        }
-      ]
-    }, {
-      featureType: 'administrative',
-      elementType: 'labels.text.fill',
-      stylers: [{
-        color: '#e85113'
-      }]
-    }, {
-      featureType: 'road.highway',
-      elementType: 'geometry.stroke',
-      stylers: [{
-          color: '#efe9e4'
-        },
-        {
-          lightness: -40
-        }
-      ]
-    }, {
-      featureType: 'transit.station',
-      stylers: [{
-          weight: 9
-        },
-        {
-          hue: '#e85113'
-        }
-      ]
-    }, {
-      featureType: 'road.highway',
-      elementType: 'labels.icon',
-      stylers: [{
-        visibility: 'off'
-      }]
-    }, {
-      featureType: 'water',
-      elementType: 'labels.text.stroke',
-      stylers: [{
-        lightness: 100
-      }]
-    }, {
-      featureType: 'water',
-      elementType: 'labels.text.fill',
-      stylers: [{
-        lightness: -100
-      }]
-    }, {
-      featureType: 'poi',
-      elementType: 'geometry',
-      stylers: [{
-          visibility: 'on'
-        },
-        {
-          color: '#f0e4d3'
-        }
-      ]
-    }, {
-      featureType: 'road.highway',
-      elementType: 'geometry.fill',
-      stylers: [{
-          color: '#efe9e4'
-        },
-        {
-          lightness: -25
-        }
-      ]
-    }];
-    // Constructor creates a new map - only center and zoom are required.
-map = new google.maps.Map(document.getElementById('map'), {
-      center: {
-        lat: 37.743333,
-        lng: -119.575833
-      },
-      zoom: 12,
-      styles: styles,
-      mapTypeControl: false
+//apply ko bindings for UI
+var AppViewModel = function(){
+    var self = this;
+    this.visibleMarkers = ko.observableArray([]);
+
+    markers.forEach(function(location){
+        self.visibleMarkers.push({title: location.title, position: location.position});
     });
 
-    // This autocomplete is for use in the search within time entry box.
-    var timeAutocomplete = new google.maps.places.Autocomplete(
-      document.getElementById('search-within-time-text'));
-    // This autocomplete is for use in the geocoder entry box.
-    var zoomAutocomplete = new google.maps.places.Autocomplete(
-      document.getElementById('zoom-to-area-text'));
-    // Bias the boundaries within the map for the zoom to area text.
-    zoomAutocomplete.bindTo('bounds', map);
-    // Normally we'd have these in a database instead.
-    var largeInfowindow = new google.maps.InfoWindow();
-    // Style the markers a bit. This will be our listing marker icon.
-    var defaultIcon = makeMarkerIcon('0091ff');
-    // Create a "highlighted location" marker color for when the user
-    // mouses over the marker.
-    var highlightedIcon = makeMarkerIcon('FFFF24');
-    // The following group uses the location array to create an array of markers on initialize.
+    //filterValue is the string to filter the visible markers with
+    this.filterValue = ko.observable();
 
-      for (var i = 0; i < locations.length; i++) {
-      // Get the position from the location array.
-      var position = locations[i].location;
-      var title = locations[i].title;
-      // Create a marker per location, and put into markers array.
-      var marker = new google.maps.Marker({
-        position: position,
-        title: title,
-        animation: google.maps.Animation.DROP,
-        icon: defaultIcon,
-        id: i
-      });
-      // Push the marker to our array of markers.
-   markers.push(marker);
-      // Create an onclick event to open the large infowindow at each marker.
-/* jshint ignore:start */
-   marker.addListener('click', function() {
-      populateInfoWindow(this, largeInfowindow);
-    });
-    // Two event listeners - one for mouseover, one for mouseout,
-    // to change the colors back and forth.
-    marker.addListener('mouseover', function() {
-      this.setIcon(highlightedIcon);
-    });
-    marker.addListener('mouseout', function() {
-      this.setIcon(defaultIcon);
-    });
-/* jshint ignore:end */
-  }
+    //updates the visible markers based on the filter
+    this.filterArray = function() {
+        var str = self.filterValue().toLowerCase();
+        this.visibleMarkers.removeAll();
+        for (i=0; i<allMarkers.length; i++) {
+            if (allMarkers[i].title.indexOf(str) != '-1'){
+                self.visibleMarkers.push(
+                    {
+                    title: allMarkers[i].title,
+                    position: allMarkers[i].position
+                    });
+                allMarkers[i].setMap(map);
+            } else {
+                allMarkers[i].setMap(null);
+            }
+        };
+    }
+
+    //clears filter to show all markers
+    this.clearFilter = function() {
+        this.visibleMarkers.removeAll();
+        markers.forEach(function(location){
+            self.visibleMarkers.push({title: location.title, position: location.position});
+        });
+        self.filterValue('');
+        for (i=0; i<allMarkers.length; i++) {
+            allMarkers[i].setMap(map);
+        };
+    };
+
+    //toggles the marker animation when the associated list item is clicked
+    this.toggleListBounce = function(s) {
+        for (i=0; i<allMarkers.length; i++) {
+            if (allMarkers[i].title === s.title) {
+                toggleMarker(allMarkers[i]);
+            }
+        }
+    }
 }
 
-  document.getElementById('show-listings').addEventListener('click', showListings);
-  document.getElementById('hide-listings').addEventListener('click', function() {
-    hideMarkers(markers);
-  });
+//initialize the map with markers
+function initMap() {
+    var self = this;
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 37.743333, lng: -119.575833},
+      zoom: 12
+    });
 
-  // This function populates the infowindow when the marker is clicked. We'll only allow
-  // one infowindow which will open at the marker that is clicked, and populate based
-  // on that markers position.
-  function populateInfoWindow(marker, infowindow) {
-    // Check to make sure the infowindow is not already opened on this marker.
-    if (infowindow.marker != marker) {
-      // Clear the infowindow content to give the streetview time to load.
-      infowindow.setContent('');
-      infowindow.marker = marker;
-      // Make sure the marker property is cleared if the infowindow is closed.
-      infowindow.addListener('closeclick', function() {
-        infowindow.marker = null;
-      });
-    }
-      var streetViewService = new google.maps.StreetViewService();
-      var radius = 50;
-      // In case the status is OK, which means the pano was found, compute the
-      // position of the streetview image, then calculate the heading, then get a
-      // panorama from that and set the options
-      function getStreetView(data, status) {
-        if (status == google.maps.StreetViewStatus.OK) {
-          var nearStreetViewLocation = data.location.latLng;
-          var heading = google.maps.geometry.spherical.computeHeading(
-            nearStreetViewLocation, marker.position);
-          infowindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>');
-          var panoramaOptions = {
-            position: nearStreetViewLocation,
-            pov: {
-              heading: heading,
-              pitch: 30
-            }
-          };
-          var panorama = new google.maps.StreetViewPanorama(
-            document.getElementById('pano'), panoramaOptions);
-        } else {
-          infowindow.setContent('<div>' + marker.title + '</div>' +
-            '<div>No Street View Found</div>');
-        }
-      }
-      // Use streetview service to get the closest streetview image within
-      // 50 meters of the markers position
-      streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
-      // Open the infowindow on the correct marker.
-      infowindow.open(map, marker);
-    }
-  // This function will loop through the markers array and display them all.
-  function showListings() {
-    var bounds = new google.maps.LatLngBounds();
-    // Extend the boundaries of the map for each marker and display the marker
-    for (var i = 0; i < markers.length; i++) {
-      markers[i].setMap(map);
-      bounds.extend(markers[i].position);
-    }
-    map.fitBounds(bounds);
-  }
-  // This function will loop through the listings and hide them all.
-  function hideMarkers(markers) {
-    for (var i = 0; i < markers.length; i++) {
-      markers[i].setMap(null);
-    }
-  }
-  // This function takes in a COLOR, and then creates a new marker
-  // icon of that color. The icon will be 21 px wide by 34 high, have an origin
-  // of 0, 0 and be anchored at 10, 34).
-  function makeMarkerIcon(markerColor) {
-    var markerImage = new google.maps.MarkerImage(
-      'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor +
-      '|40|_|%E2%80%A2',
-      new google.maps.Size(21, 34),
-      new google.maps.Point(0, 0),
-      new google.maps.Point(10, 34),
-      new google.maps.Size(21, 34));
-    return markerImage;
-  }
 
-  //make marker bounce if "clicked" on sidebar
-  function toggleBounce0() {
-    if (markers[0].getAnimation() !== null) {
-      markers[0].setAnimation(null);
-    } else {
-      markers[0].setAnimation(google.maps.Animation.BOUNCE);
-    }
-  }
 
-  function toggleBounce1() {
-    if (markers[1].getAnimation() !== null) {
-      markers[1].setAnimation(null);
-    } else {
-      markers[1].setAnimation(google.maps.Animation.BOUNCE);
-    }
-  }
-
-  function toggleBounce2() {
-    if (markers[2].getAnimation() !== null) {
-      markers[2].setAnimation(null);
-    } else {
-      markers[2].setAnimation(google.maps.Animation.BOUNCE);
-    }
-  }
-
-  function toggleBounce3() {
-    if (markers[3].getAnimation() !== null) {
-      markers[3].setAnimation(null);
-    } else {
-      markers[3].setAnimation(google.maps.Animation.BOUNCE);
-    }
-  }
-
-  function toggleBounce4() {
-    if (markers[4].getAnimation() !== null) {
-      markers[4].setAnimation(null);
-    } else {
-      markers[4].setAnimation(google.maps.Animation.BOUNCE);
-    }
-  }
-
-  function toggleBounce5() {
-    if (markers[5].getAnimation() !== null) {
-      markers[5].setAnimation(null);
-    } else {
-      markers[5].setAnimation(google.maps.Animation.BOUNCE);
-    }
-  }
-
-  googleError = function googleError() {
-        alert(
-            'It looks like Google Maps faild to load. Please refresh the page and try again!'
-        );
+    for (i=0; i<markers.length; i++){
+        var markerInfo = (function(markers){
+            var marker = new google.maps.Marker({
+                title: markers.title,
+                position: markers.position,
+                id: markers.id,
+                map: map
+            });
+            marker.setAnimation(null);
+            marker.addListener('click', function() {
+                toggleMarker(marker);
+            });
+            allMarkers.push(marker);
+        })(markers[i]);
     };
+
+this.toggleMarker = function(marker) {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout(function(){ marker.setAnimation(null); }, 2500);
+    };
+
+//Add Infowindow to the site
+  var infowindow = new google.maps.InfoWindow({
+      content:"Hello World!"
+      });
+
+  google.maps.event.addListener(markers, 'click', function() {
+      infowindow.open(map,markers);
+      });
+   }
+
+
+    function makeMarkerIcon(markerColor) {
+      var markerImage = new google.maps.MarkerImage(
+        'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor +
+        '|40|_|%E2%80%A2',
+        new google.maps.Size(21, 34),
+        new google.maps.Point(0, 0),
+        new google.maps.Point(10, 34),
+        new google.maps.Size(21, 34));
+      return markerImage;
+    }
+
+
+//error message if google map api not displaying correctly
+googleError = function googleError() {
+      window.alert(
+          'It looks like Google Maps faild to load. Please refresh the page and try again!'
+      );
+  };
+
+//replace all function
+String.prototype.replaceAll = function(target, replacement) {
+    return this.split(target).join(replacement);
+}
+
+//ko initiate function to apply ko bindings.
+ko.applyBindings(new AppViewModel());
